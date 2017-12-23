eee5c0de21f43c01a02ea6430f2fcbc66c5c9227
Abdullah Hashim <abdullah97Hashim@gmail.com>
2017-12-23 18:39:36 +0300

    first commit

--- index.js
+++ index.js
@@ -2,25 +2,104 @@ var express = require('express');
 var socket = require('socket.io');
 //App setup
 var app = express();
-var server = app.listen(4000,function(){
+var server = app.listen(4000,'192.168.1.129',function(){
   console.log("Listening to requests on port 4000")
+});
+
+
+var fs = require('fs');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({	extended: true })); // support encoded bodies
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+// POST http://localhost:8080/api/users
+// parameters sent with 
+
+app.post('/', function(req, res) {
+	var user_id = req.body;
+	console.log(user_id);
 });
 
 //Static files
 app.use(express.static('public'));
-
+var users = [];
+var list = {
+    handleName:'',
+    id:''
+};
+var arr =[];
 //Socket setup
 var io = socket(server);
-
 io.on('connection',function(socket){
-  console.log('made socket connection', socket.id);
+    console.log('made socket connection', socket.id);
+   
+    socket.on('check', function(data){
+        io.sockets.emit('check', data);
+    });
+    
+    socket.on('online', function(data){
+       if(users.indexOf(data) === -1) {
+            users.push(data);
+            console.log(data);
+        }
+        io.sockets.emit('online', users);
+    }); 
+    
+    socket.on('offline', function(data){
+        console.log(data + '- disconnect');
+        for(var i=0; i < users.length; i++) {
+           if(users[i] == data){
+              users.splice(i,1);
+           }
+        }
+        console.log(users);
+            io.sockets.emit('offline', users);
+    }); 
+    
+    socket.on('chat', function(data){
+         var time = myDate.getHours() + ":" + myDate.getMinutes() + ":" + myDate.getSeconds();
 
+        fs.appendFileSync('public/messages.txt', data.handle + ': ' +data.message + ' ('+ time + ')\r\n');
+        io.sockets.emit('chat', data);
+    });
 
-  socket.on('chat', function(data){
-    io.sockets.emit('chat', data);
-  });
+    socket.on('typing',function(data){
+        socket.broadcast.emit('typing',data);
+    });
+    
+myDate = new Date(); 
 
-  socket.on('typing',function(data){
-    socket.broadcast.emit('typing',data);
-  });
+    socket.on('tabs',function(data){
+        var time = myDate.getHours() + ":" + myDate.getMinutes() + ":" + myDate.getSeconds();
+
+        fs.appendFileSync('public/links.txt',time +' time of first link shared' + '\r\n \r\n');
+        data.forEach((link,i)=>{
+            fs.appendFileSync('public/links.txt',i+1 + '- '+ link + '\r\n \r\n');
+        });
+//        for(var i =0;i<arr.length;i++){
+//            if(socket.id === arr[i].id)
+//                console.log(arr[i].handleName);
+//        }
+        io.sockets.emit('print', data);
+    });
 });
+
--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,9 @@
 //Make connection
-var socket = io.connect('http://localhost:4000');
+var socket = io.connect('http://192.168.1.129:4000');
+
+socket.on('check', function(message) {
+    console.log(message);
+});
 
 //Query DOM
 var message = document.getElementById('message');
@@ -7,26 +11,98 @@ var message = document.getElementById('message');
     btn = document.getElementById('send');
     output = document.getElementById('output');
     feedback = document.getElementById('feedback');
+    feedback1 = document.getElementById('feedback1');
+
+
+    name = window.location.search.split('=')[1];
+    if(name.includes('%'))
+        handleName = 'user';
+    else 
+        handleName =name;
+    handle.value = handleName;
+
+ socket.on('connect', function() {
+     socket.emit('online',handleName);
+ });
+
+$('#disconnect').click(function(){
+    console.log('disconnect');
+    socket.emit('offline',handleName);
+    socket.disconnect();
+});
 
 //Emit events
+btn.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        btn.click();
+    }
+});
 btn.addEventListener('click',function(){
-  socket.emit('chat',{
-    message: message.value,
-    handle:handle.value
-  });
+    if(message.value != ""){
+        socket.emit('chat',{
+            message: message.value,
+            handle: handleName
+        });
+    }
+    message.value = '';
+    myDate = new Date();
+    time = myDate.getHours() + ":" + myDate.getMinutes() + ":" + myDate.getSeconds();
+
 });
 
 message.addEventListener('keypress',function(){
-  socket.emit('typing',handle.value);
-})
+    socket.emit('typing',handle.value);
+});
 
-//Listen for events
+myDate = new Date(); 
+var time = myDate.getHours() + ":" + myDate.getMinutes() + ":" + myDate.getSeconds();
+
+
 socket.on('chat', function(data){
+    console.log(data);
   feedback.innerHTML = ""
-  output.innerHTML += '<p><strong>'+data.handle + ':</strong>' + data.message + '</p>';
+  output.innerHTML += '<p><strong>'+data.handle +
+                      ':</strong>' + data.message +"<sub id = 'time'>"+time+'</sub>' +'</p>' ;
 });
 
 socket.on('typing',function(data){
-
   feedback.innerHTML = '<p><em>' + data + 'is typing a mesage...</em></p>';
 });
+
+socket.on('online',function(data){
+    var users = [];
+    data.forEach((user)=>{
+        users.push(user + ' is connected' + '<br>');
+    });
+    if(users[0] == undefined) users[0]="";
+    if(users[1] == undefined) users[1]="";
+    document.getElementById('output2').innerHTML = users[0] + users[1] ;
+
+});
+socket.on('offline',function(data){
+    data.forEach((user)=>{
+        document.getElementById('output2').innerHTML = user + ' is connected' + '<br>';
+    }); 
+});
+
+socket.on('print',function(data){
+feedback1.innerHTML += '<hr>';
+    data.forEach((element,i)=>{
+       feedback1.innerHTML +='<p><strong>'+ (i+1)  +":</strong> <a href='"+element+"'><em>" + element + '</p>'; 
+        updateScroll();
+    });
+});
+
+function updateScroll(){
+    var element = document.getElementById("links-window");
+    element.scrollTop = element.scrollHeight;
+}
+
+function arrBox(){
+    var arr = document.getElementById('arrBox');
+    arr.style.display='';
+    arr.onmouseover= ()=>{
+        arr.style.display='none';
+    }
+}
